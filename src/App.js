45563c7b9f8455e26ab22bad585a7ff60b851bc5
ANURAG AGARWAL <anuragagarwal203@gmail.com>
2022-07-15 02:43:10 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import About from './components/About';
+import React, { useState } from 'react'
+import Alert from './components/Alert';
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+// } from "react-router-dom";
+
+function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+  function toggleMode() {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "black";
+      showAlert("Dark mode has been enabled", "success");
+    }
+    else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success");
+    }
+  };
+  function showAlert(message, type) {
+    setAlert({
+      msg: message,
+      type: type
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+  return (
+    <>
+      {/* <Router>
+        <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <Routes>
+          <Route exact path="/" element={<TextForm heading="Enter the test to analyze below:" mode={mode} showAlert={showAlert} />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </Router> */}
+      <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <TextForm heading="Enter the test to analyze below:" mode={mode} showAlert={showAlert} />
+      <About/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/TextForm.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react'
+
+export default function TextForm(props) {
+    const [text, setText] = useState("");
+    function upperCase() {
+        // console.log("Uppercase was clicked");
+        let newUppertext = text.toUpperCase();
+        setText(newUppertext);
+        props.showAlert("Converted to UpperCase!","success");
+    };
+    function lowerCase() {
+        // console.log("Uppercase was clicked");
+        let newUppertext = text.toLowerCase();
+        setText(newUppertext);
+        props.showAlert("Converted to LowerCase!","success");
+    };
+    function capitalizeEachWord() {
+        let newUppertext = "";
+        for (let i = 0; i < text.length; i++) {
+            if (i === 0 || text[i - 1] === " ") {
+                newUppertext += text[i].toUpperCase();
+            }
+            else newUppertext += text[i];
+        }
+        setText(newUppertext);
+        props.showAlert("Capatilized Each word!","success");
+    };
+    function copyText() {
+        navigator.clipboard.writeText(text);
+        props.showAlert("Text Copied to Clipboard!","success");
+    }
+    function removeExtraSpaces() {
+        let newUppertext = "";
+        for (let i = 0; i < text.length; i++) {
+            if ((i === 0 || text[i - 1] === " ") && text[i] === " ") {
+                continue;
+            }
+            else newUppertext += text[i];
+        }
+        setText(newUppertext);
+        props.showAlert("Removed Extra Spaces!","success");
+    };
+    function clearAll() {
+        // console.log("Uppercase was clicked");
+        setText("");
+        props.showAlert("Cleared All!","success");
+    };
+    function handleOnChange(event) {
+        // console.log("on change");
+        setText(event.target.value);
+    };
+    function len_words(){
+        return text.trim().split(" ").length;
+    };
+    return (
+        <>
+            <div className="container my-4">
+                <h1 className='fourth' style={{ color: props.mode === "light" ? "black" : "white" }}>{props.heading}</h1>
+                <div className="mb-3 my-4" >
+                    {/* <label for="myBox" className="form-label">{props.heading}</label> */}
+                    <textarea className="form-control" id="myBox" rows="10" style={{ backgroundColor: props.mode === "light" ? "white" : "black", color: props.mode === "light" ? "black" : "white" }} onChange={handleOnChange} value={text} placeholder='Enter the text...' ></textarea>
+                </div>
+                <button className="btn btn-primary mx-2" onClick={upperCase}>Convert to Upppercase</button>
+                <button className="btn btn-secondary mx-2" onClick={lowerCase}>Convert to Lowercase</button>
+                <button className="btn btn-danger mx-2" onClick={capitalizeEachWord}>Capitalize Every Word</button>
+                <button className="btn btn-warning mx-2" onClick={copyText}>Copy Text</button>
+                <button className="btn btn-info mx-2" onClick={removeExtraSpaces}>Remove Extra Spaces</button>
+                <button className="btn btn-dark mx-2" onClick={clearAll}>Clear All</button>
+            </div>
+            <div className="container my-3">
+                <div className="second">
+                    <h2 className='third'>Your text summary</h2>
+                    <p className='first'>{len_words()} words, {text.length} characters </p>
+                    <p className='first'>{(0.008 * text.split(" ").length).toFixed(4)} minutes to read</p>
+                </div>
+                <div className="secondd" style={{ color: props.mode === "light" ? "black" : "white" }}>
+                    <h2 className='third'>Preview</h2>
+                    <p className='my-4 text-muted'>{text.length > 0 ? text : "Enter something in the textbox to preview it here..."}</p>
+                </div>
+            </div>
+        </>
+    )
+}
