6623cd9a1d479df1261cecddcd2ac8f87d5d0f0c
ANURAG AGARWAL <anuragagarwal203@gmail.com>
2022-07-15 04:01:37 +0530

    Updates

---
+++ precache-manifest.67fd151bfb1f43017e1723a94fa21968.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d8744fa4bb92966fddd",
+    "url": "/TextUtils-React/static/css/main.cb955d97.chunk.css"
+  },
+  {
+    "revision": "4d8744fa4bb92966fddd",
+    "url": "/TextUtils-React/static/js/main.4d8744fa.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtils-React/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextUtils-React/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "54d4ef26c5198954da45",
+    "url": "/TextUtils-React/static/js/runtime~main.54d4ef26.js"
+  },
+  {
+    "revision": "6251a86094edf63efebcef6eb0d3b2b3",
+    "url": "/TextUtils-React/index.html"
+  }
+];
